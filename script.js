8ab6da3cd9ac3cadd53208525a397e453d6f625f
RemoHoesli <remo.hoesli@stud.hslu.ch>
2024-05-01 14:11:58 +0200

    timer

---
+++ script.js
@@ -0,0 +1,18 @@
+
+
+
+var timeLeft = 30;
+    var elem = document.getElementById('some_div');
+    
+    var timerId = setInterval(countdown, 1000);
+    
+    function countdown() {
+      if (timeLeft == -1) {
+        clearTimeout(timerId);
+        doSomething();
+      } else {
+        console.log(timeLeft+' seconds remaining');
+        timeLeft--;
+      }
+      console.log(timeLeft);
+    }
