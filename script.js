7c7eb4b5b5f531b7235f3fbeaf45e589fe7268b2
RemoHoesli <remo.hoesli@stud.hslu.ch>
2024-05-02 10:26:46 +0200

    minigame

--- script.js
+++ script.js
@@ -1,22 +1,37 @@
 document.addEventListener('DOMContentLoaded', function () {
 
-    
-    var timeLeft = 5;
-    const rating = 4.0;
-    
-        var timerId = setInterval(countdown, 1000);
+    document.getElementById("button").addEventListener("click", function() {
+
+        const spam = document.getElementById("spam");
+        button.style.transition = "2s";
+        button.style.opacity = "0%";
+        spam.style.transition = "2s";
+        spam.style.opacity = "100%";
+        
+        var timeLeft = 5;
+        var rating = 40;
+        
+        var timerId = setInterval(countdown, 100);
     
         function countdown() {
-        if (timeLeft == 0) {
-            clearTimeout(timerId);
-            document.getElementById("Countdown").innerHTML = 'bruh';
-        } else {
-            console.log(timeLeft+' seconds remaining');
-            timeLeft--;
-            document.getElementById("Countdown").innerHTML = 'Time left ' + timeLeft;
+            if (timeLeft < 0) {
+                clearTimeout(timerId);
+                document.getElementById("Countdown").innerHTML = 'bruh';
+            } else {
+                document.getElementById("spam").addEventListener("click", function() {
+                    if (rating < 48) {
+                    rating += 0.3; 
+                }
+                });
+                console.log(timeLeft+' seconds remaining');
+                timeLeft -= 0.1;
+                rating /= 1.2;
+                document.getElementById("Countdown").innerHTML = 'Time left ' + Math.ceil(timeLeft);
+                var ratingend = rating / 10 + 1;
+                document.getElementById("rat").innerHTML = ratingend.toFixed(2);
+            }
         }
-    }
 
-    document.getElementById("rat").innerHTML = rating;
+    });
 
 });
