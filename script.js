9eff9a20064f25f290779d90cba88de3d52d6920
RemoHoesli <remo.hoesli@stud.hslu.ch>
2024-05-07 06:25:34 +0200

    pretty much done

--- script.js
+++ script.js
@@ -16,25 +16,66 @@ document.addEventListener('DOMContentLoaded', function () {
     document.getElementById("second").innerHTML = '2) '+second.toFixed(2);
     document.getElementById("third").innerHTML = '3) '+third.toFixed(2);
 
-    document.getElementById("button").addEventListener("click", function() {
+    const spam = document.getElementById("spam");
+    const pre = document.querySelectorAll('.pre');
+    const during = document.querySelectorAll('.during');
+    const post = document.querySelectorAll('.post');
 
-        const spam = document.getElementById("spam");
+    document.getElementById("startbutton").addEventListener("click", function() {
 
-        button.style.transform = "translateX(100vw)";
-        spam.style.transform = "translateX(0vw)";
+        
+        pre.forEach(function(element) {
+            element.style.transform = "translateX(100vw)"; });
+        during.forEach(function(element) {
+            element.style.transform = "translateX(0vw)"; });
+        document.getElementById("Countdown").style.opacity = '0%';
 
-        var timeLeft = 10;
+        var timeLeft = 13;
         var rating = 0;        
         
         
         var timerId = setInterval(countdown, 50);
     
         function countdown() {
+            if (timeLeft < 12) {
+                document.getElementById("starttimer").innerHTML = '2';
+
+            }
+            if (timeLeft < 11) {
+                document.getElementById("starttimer").innerHTML = '1';
+
+            }
+            if (timeLeft < 10) {
+                document.getElementById("starttimer").style.transform = 'translatex(100vw)';
+                document.getElementById("starttimercircle").style.transform = 'translatex(100vw)';
+                document.getElementById("Countdown").style.opacity = '100%';
+            }
             if (timeLeft < 0) {
                 clearTimeout(timerId);
-                document.getElementById("Countdown").innerHTML = 'inspirational quote';
-                button.style.transform = "translateX(0vw)";
-                spam.style.transform = "translateX(100vw)";
+                if (ratingend > 0) {
+                    document.getElementById("quote").innerHTML = '"Lets have a little fun here. You know, screw the planet"';
+                }
+                if (ratingend > 2.0) {
+                    document.getElementById("quote").innerHTML = '"I dont know whats up with you but I cannot have a 2 at my wedding"';
+                }
+                if (ratingend > 3.0) {
+                    document.getElementById("quote").innerHTML = '"Trying to scrape yourself back? If its below that, then its bye bye"';
+                }
+                if (ratingend > 3.5) {
+                    document.getElementById("quote").innerHTML = '"It turned out a lot of my friends didnt care for honesty"';
+                }
+                if (ratingend > 4.2) {
+                    document.getElementById("quote").innerHTML = '"You know our Prime Influencers Programme? Wed need you around a 4.5"';
+                }
+                if (ratingend > 4.5) {
+                    document.getElementById("quote").innerHTML = '"You could use a punch up right there. Thats upvotes from quality people"';
+                }     
+                
+                
+                during.forEach(function(element) {
+                    element.style.transform = "translateX(100vw)"; });
+                post.forEach(function(element) {
+                    element.style.transform = "translateX(0vw)"; });
 
                 ratingfinal = ratingend;
                 if (ratingfinal > first) {
@@ -44,10 +85,17 @@ document.addEventListener('DOMContentLoaded', function () {
                 } else if (ratingfinal > third) {
                     third = ratingfinal;
                 }
-                document.getElementById("rat").innerHTML = ratingend.toFixed(2);
+                document.getElementById("yourat").innerHTML = ratingend.toFixed(2);
                 document.getElementById("first").innerHTML = '1) '+first.toFixed(2);
                 document.getElementById("second").innerHTML = '2) '+second.toFixed(2);
                 document.getElementById("third").innerHTML = '3) '+third.toFixed(2);
+
+                document.getElementById("backbutton").addEventListener("click", function() {
+                    post.forEach(function(element) {
+                        element.style.transform = "translateX(100vw)"; });
+                    pre.forEach(function(element) {
+                        element.style.transform = "translateX(0vw)"; });
+                });
             } else {
                 document.getElementById("spam").addEventListener("click", function() {
                     if (rating < 50) {
@@ -57,7 +105,7 @@ document.addEventListener('DOMContentLoaded', function () {
                 console.log(timeLeft+' seconds remaining');
                 timeLeft -= 0.05;
                 rating *= 0.95;
-                document.getElementById("Countdown").innerHTML = 'Time left ' + Math.ceil(timeLeft);
+                document.getElementById("Countdown").innerHTML = 'Time left: ' + Math.ceil(timeLeft) + 's';
                 ratingend = rating / 10 + 1;
                 document.getElementById("rat").innerHTML = ratingend.toFixed(1);
             }
